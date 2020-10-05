54b0ce52f582643087e78dd917714107819adcd7
brianayon <brianayon19@gmail.com>
2020-10-04 23:08:43 -0700

    Initial commit

---
+++ AddingMachine/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,52 @@
+package cse360assignment02;
+
+/**
+ * Create class AddingMachine
+ */
+public class AddingMachine {
+    private int total;
+
+    /**
+     * Class constructor for AddingMachine
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+
+    /**
+     * Method to return total
+     * @return 0
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /**
+     * Method to add
+     * @param value
+     */
+    public void add (int value) {
+    }
+
+    /**
+     * Method to subtract
+     * @param value
+     */
+    public void subtract (int value) {
+    }
+
+    /**
+     * Method to return string
+     * @return
+     */
+    public String toString () {
+        return "";
+    }
+
+    /**
+     * Method to clear
+     */
+    public void clear() {
+    }
+}
