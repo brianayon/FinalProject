628256f2f7936a027d9050b5556ba03fb65126a8
brianayon <brianayon19@gmail.com>
2020-10-06 15:20:31 -0700

    Add a simple change.

--- AddingMachine/src/cse360assignment02/AddingMachine.java
+++ AddingMachine/src/cse360assignment02/AddingMachine.java
@@ -1,3 +1,6 @@
+/*
+ * @author	Brian Ayon
+ */
 package cse360assignment02;
 
 /**
@@ -5,21 +8,23 @@ package cse360assignment02;
  */
 public class AddingMachine {
     private int total;
+    private String totalString;
 
     /**
      * Class constructor for AddingMachine
      */
     public AddingMachine () {
         total = 0;  // not needed - included for clarity
+        totalString = "";
     }
 
 
     /**
      * Method to return total
-     * @return 0
+     * @return total
      */
     public int getTotal () {
-        return 0;
+        return total;
     }
 
     /**
@@ -27,6 +32,8 @@ public class AddingMachine {
      * @param value
      */
     public void add (int value) {
+    	total = total + value;
+    	totalString += " + " + value;
     }
 
     /**
@@ -34,19 +41,23 @@ public class AddingMachine {
      * @param value
      */
     public void subtract (int value) {
+    	total = total - value;
+    	totalString += " - " + value;
     }
 
     /**
      * Method to return string
-     * @return
+     * @return totalString
      */
     public String toString () {
-        return "";
+        return totalString;
     }
 
     /**
      * Method to clear
      */
     public void clear() {
+    	total = 0;
+    	totalString = "";
     }
 }
